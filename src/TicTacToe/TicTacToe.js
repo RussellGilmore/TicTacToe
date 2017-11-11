395f5a013daeae806cb6bd7b3b528373615e5a4b
Russell <RussellGilmore@Live.com>
2017-11-10 22:37:19 -0500

    Added Button to clear game board.

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -16,45 +16,25 @@ const translatePieces = (piece) => {
 };
 
 
-const VictoryModal = () => (
-    <div id="potato" className="modal fade">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Victory!</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <p>Modal body text goes here.</p>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-primary">New Game?</button>
-          </div>
-        </div>
-      </div>
-    </div>);
-// const modalInstance = (
-//   <div className="static-modal">
-//     <Modal.Dialog>
-//       <Modal.Header>
-//         <Modal.Title>Modal title</Modal.Title>
-//       </Modal.Header>
-//
-//       <Modal.Body>
-//         One fine body...
-//       </Modal.Body>
-//
-//       <Modal.Footer>
-//         <Button>Close</Button>
-//         <Button bsStyle="primary">Save changes</Button>
-//       </Modal.Footer>
-//
-//     </Modal.Dialog>
-//   </div>
-// );
-
+// const VictoryModal = () => (
+//     <div id="potato" className="modal fade">
+//       <div className="modal-dialog" role="document">
+//         <div className="modal-content">
+//           <div className="modal-header">
+//             <h5 className="modal-title">Victory!</h5>
+//             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//               <span aria-hidden="true">&times;</span>
+//             </button>
+//           </div>
+//           <div className="modal-body">
+//             <p>Modal body text goes here.</p>
+//           </div>
+//           <div className="modal-footer">
+//             <button type="button" className="btn btn-primary">New Game?</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>);
 
 const TicTacToeRow = ({row, rowIndex, placePiece}) => {
   return (
@@ -69,12 +49,12 @@ const TicTacToeRow = ({row, rowIndex, placePiece}) => {
     </div>
   );
 };
-
+const EMPTY_BOARD = Array(3).fill(Array(3).fill(0));
 class TicTacToe extends Component {
   constructor() {
     super();
     this.state = {
-      board: Array(3).fill(Array(3).fill(0)),
+      board: EMPTY_BOARD,
       turn: 0
     };
 
@@ -82,6 +62,8 @@ class TicTacToe extends Component {
     this.renderRow = this.renderRow.bind(this);
     this.gameState = this.gameState.bind(this);
     this.renderModal = this.renderModal.bind(this);
+    this.renderButton = this.renderButton.bind(this);
+    this.clearBoard = this.clearBoard.bind(this);
   }
 
   componentDidMount() {
@@ -89,36 +71,41 @@ class TicTacToe extends Component {
   }
 
   gameState() {
-
     for(var playerValue = 1; playerValue <=2;playerValue++){
       if (this.state.board[0][0] === playerValue && this.state.board[0][1] === playerValue && this.state.board[0][2] === playerValue) {
         console.log('e1');
+        this.clearBoard();
         this.renderModal();
       } else if (this.state.board[1][0] === playerValue && this.state.board[1][1] === playerValue && this.state.board[1][2] === playerValue) {
         console.log('e2');
+        this.clearBoard();
         this.renderModal();
       } else if (this.state.board[2][0] === playerValue && this.state.board[2][1] === playerValue && this.state.board[2][2] === playerValue) {
         console.log('e3');
+        this.clearBoard();
         this.renderModal(); // Rows End
       } else if (this.state.board[0][0] === playerValue && this.state.board[1][0] === playerValue && this.state.board[2][0] === playerValue) {
         console.log('e4');
+        this.clearBoard();
         this.renderModal();
       } else if (this.state.board[0][1] === playerValue && this.state.board[1][1] === playerValue && this.state.board[2][1] === playerValue) {
         console.log('e5');
+        this.clearBoard();
         this.renderModal();
       } else if (this.state.board[0][2] === playerValue && this.state.board[1][2] === playerValue && this.state.board[2][2] === playerValue) {
         console.log('e6');
+        this.clearBoard();
         this.renderModal(); // Columns End
       } else if (this.state.board[0][0] === playerValue && this.state.board[1][1] === playerValue && this.state.board[2][2] === playerValue) {
         console.log('e7');
+        this.clearBoard();
         this.renderModal();
       } else if (this.state.board[0][2] === playerValue && this.state.board[1][1] === playerValue && this.state.board[2][0] === playerValue) {
         console.log('e8');
+        this.clearBoard();
         this.renderModal();
       }
     }
-
-
   }
 
   placePiece(i, j) {
@@ -139,6 +126,12 @@ class TicTacToe extends Component {
     });
   }
 
+  // renderNewGameButton(){
+  //   <button onClick={clearBoard}>
+  //     Clear Board
+  //   </button>
+  // }
+
   renderRow(row, i) {
     return (<TicTacToeRow key={i} row={row} rowIndex={i} placePiece={this.placePiece}/>);
   }
@@ -147,13 +140,35 @@ class TicTacToe extends Component {
     window.$(ReactDOM.findDOMNode(this).querySelector('.modal')).modal();
   }
 
+  renderButton(){
+    return(<button onClick={this.clearBoard}>
+      Clear Board
+    </button>);
+  }
+
+  clearBoard(){
+    this.setState({
+      board: EMPTY_BOARD,
+      turn: (this.state.turn === 0
+        ? 1
+        : 0)
+    });
+  }
+
 
   render() {
     return (
-      <div className="container TicTacToe">
-        {this.state.board.map(this.renderRow)}
-        <VictoryModal />
+      <div>
+        <div className="container TicTacToe">
+          {this.state.board.map(this.renderRow)}
+        </div>
+        <div className="container clear-button">
+          <button className="btn btn-primary" onClick={this.clearBoard}>
+            Clear Board
+          </button>
+        </div>
       </div>
+
     );
   }
 }
